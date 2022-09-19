2618ba5dcf5ef41f7c21a4c49f0c2b3b87bc8129
Maryam-Momoh <marryammomoh@gmail.com>
2022-09-19 16:48:10 +0100

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const Navbar = () => {
+  return (
+    <>
+       <nav className="nav navbar">
+          <div className="navbar-container">
+            
+          </div>
+       </nav>
+    </>
+  )
+}
+
+export default Navbar
